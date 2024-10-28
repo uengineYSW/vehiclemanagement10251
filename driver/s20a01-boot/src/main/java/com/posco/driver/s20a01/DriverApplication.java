b0656a50dd92c72f008362080b18ab6d38b60716
uengineYSW <112850933+uengineYSW@users.noreply.github.com>
2024-10-28 15:59:34 +0900

    Update DriverApplication.java

--- driver/s20a01-boot/src/main/java/com/posco/driver/s20a01/DriverApplication.java
+++ driver/s20a01-boot/src/main/java/com/posco/driver/s20a01/DriverApplication.java
@@ -3,6 +3,7 @@ package com.posco.driver.s20a01;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
